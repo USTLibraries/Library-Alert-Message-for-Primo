595856eddcee6abb99d78f6bc5c915b832e2f54a
Chad Leigh Kluck <chad01@kluckfamily.net>
2017-09-05 13:19:22 -0500

    Update alert-js.js

--- alert-js.js
+++ alert-js.js
@@ -1,25 +1,61 @@
 /* place this code in your custom js */
 
-/*  ============================================================================================
-    ********************************************************************************************
-        LIBRARY ALERT
-    ********************************************************************************************
-*/
+	/* *******************************************************************
+	 *   LIBRARY ALERT MESSAGE FOR PRIMO FUNCTION
+	 */
+	
+	var libraryAlertMessage = function(alertMsg) {
 
-var ustlibraryalert = ""; // place your message in this string. if it is empty ("") no message or box will display
+		if (alertMsg !== "") {
+			setTimeout(function() {
+				var x = document.getElementsByClassName("topbar-wrapper");
 
-if (ustlibraryalert !== "") {
-	setTimeout(function() {
-		var x = document.getElementsByClassName("topbar-wrapper");
+				if ( x.length > 0) {
 
-		if ( x.length > 0) {
-			var div = document.createElement("DIV");
-			div.className += 'ust-library-alert ';
-			div.className += 'advisory'; // help/informational(blue) normal/success(green) advisory/warning(yellow) critical/error(red)
-			var innerdiv = document.createElement("DIV");
-			innerdiv.innerHTML = ustlibraryalert;
-			div.appendChild(innerdiv);
-			x[0].appendChild(div);
+					var level = "red-critical-error";
+
+					if ( typeof alertMsg.includes !== "undefined" ) { // .includes is not supported in older browsers
+
+						if (alertMsg.includes("[blue]") ) { level = "blue-help-informational"; }
+						else if (alertMsg.includes("[green]") ) { level = "green-success-normal"; }
+						else if (alertMsg.includes("[yellow]") ) { level = "yellow-advisory-warning"; }
+						// default is critical or error which is already set
+					} else {
+						console.log("INIT: Alert is default critical because browser does not support .includes()");
+					}
+
+					alertMsg = alertMsg.replace(/\[[a-z]*\]/ig, "");
+
+					var div = document.createElement("DIV");
+					div.className += 'library-alert-message ';
+					div.className += level;
+					var innerdiv = document.createElement("DIV");
+					innerdiv.innerHTML = alertMsg;
+					div.appendChild(innerdiv);
+					x[0].appendChild(div);
+				}
+			}, 8000);
+		} else {
+			console.log("INIT: No current alerts");
 		}
-	}, 8000);
-}
+	};
+	
+
+	/* *******************************************************************
+	 *  SET LIBRARY ALERT MESSAGE FOR PRIMO
+	 *
+	 *  INSTRUCTIONS:
+	
+		pre-pend alert level color in front of text. HTML ok
+	
+		libraryAlertMessage(""); // no alert to display
+
+		libraryAlertMessage("[blue]Your help/informational message goes here");
+		libraryAlertMessage("[green]Your normal/success message goes here");
+		libraryAlertMessage("[yellow]Your advisory/warning message goes here <a href='somepage.html'>More info</a>");
+		libraryAlertMessage("[red]Your critical/error message goes here");
+		
+		Default is red/critical so pre-pending with [red] is not necessary.
+	*/
+ 
+	libraryAlertMessage("");
